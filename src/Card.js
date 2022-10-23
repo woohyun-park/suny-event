7d47a35299f4f6535fabd499c5364052e9b56822
Woohyun Park <woohyunpark.mail@gmail.com>
2022-10-23 17:01:08 +0900

    ADD, add ongoing, upcoming, finished

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,26 @@
 import "./Card.css";
+import styled from "styled-components";
 
 function Card({ data }) {
   return (
-    <div className="card">
-      <div className="card__firstCont">
-        <div className="card__title">{data.title}</div>
+    <Cont color={data.color} className="card">
+      <div className="card__title">{data.title}</div>
+      <div className="card__cont">
         <div className="card__txt">{data.txt}</div>
-      </div>
-      <div className="card__secondCont">
-        <div className="card__time">
-          {data.time1}
-          <br />
-          {data.time2}
+        <div className="card__secondCont">
+          <div className="card__time">
+            {data.time1}
+            <br />
+            {data.time2}
+          </div>
         </div>
       </div>
-    </div>
+    </Cont>
   );
 }
+
+const Cont = styled.div`
+  background-color: ${(props) => props.color};
+`;
 
 export default Card;
--- src/Event.js
+++ src/Event.js
@@ -2,15 +2,37 @@ import { useState } from "react";
 import Card from "./Card";
 import "./Event.css";
 
-const tempCard = {
-  title: "BINGO TIME",
-  txt: "play bingo and earn prizes",
-  time1: "22.10.12",
-  time2: "12:30-13:30",
+const events = {
+  ongoing: [
+    {
+      title: "Bingo Time",
+      txt: "play bingo and earn prizes",
+      time1: "22.10.12",
+      time2: "12:30-13:30",
+      color: "#0A356A",
+    },
+  ],
+  upcoming: [
+    {
+      title: "Happy Light Tree",
+      txt: "make a wish tree and write secret letters",
+      time1: "",
+      time2: "22.10.12 - 22.10.14",
+      color: "#E0377A",
+    },
+  ],
+  finished: [],
 };
 
 function Event() {
-  const buttons = ["grid", "tag", "scrap"];
+  const getButtons = () => {
+    let result = [];
+    for (let each in events) {
+      result.push(each);
+    }
+    return result;
+  };
+  const [buttons, setButtons] = useState(getButtons());
   const [pageNum, setPageNum] = useState(0);
   return (
     <div className="event">
@@ -30,7 +52,9 @@ function Event() {
         ))}
       </div>
       <div className="event__cont">
-        <Card data={tempCard} />
+        {events[buttons[pageNum]].map((each) => (
+          <Card data={each} />
+        ))}
       </div>
     </div>
   );
